e39023ea82c8eb351238a2eb7d798d8b1ef1cf78
zraffens <abhinavacharya486@gmail.com>
2023-07-11 22:40:56 +0545

    winner checking func done

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -13,11 +13,75 @@ function createBoard() {
   return board;
 }
 
+function checkWin(board) {
+  // Check rows
+  for (let row = 0; row < 6; row++) {
+    for (let col = 0; col < 4; col++) {
+      if (
+        board[row][col] &&
+        board[row][col] === board[row][col + 1] &&
+        board[row][col] === board[row][col + 2] &&
+        board[row][col] === board[row][col + 3]
+      ) {
+        return board[row][col];
+      }
+    }
+  }
+
+  // Check columns
+  for (let col = 0; col < 7; col++) {
+    for (let row = 0; row < 3; row++) {
+      if (
+        board[row][col] &&
+        board[row][col] === board[row + 1][col] &&
+        board[row][col] === board[row + 2][col] &&
+        board[row][col] === board[row + 3][col]
+      ) {
+        return board[row][col];
+      }
+    }
+  }
+
+  // Check diagonals (top-left to bottom-right)
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 4; col++) {
+      if (
+        board[row][col] &&
+        board[row][col] === board[row + 1][col + 1] &&
+        board[row][col] === board[row + 2][col + 2] &&
+        board[row][col] === board[row + 3][col + 3]
+      ) {
+        return board[row][col];
+      }
+    }
+  }
+
+  // Check diagonals (bottom-left to top-right)
+  for (let row = 5; row >= 3; row--) {
+    for (let col = 0; col < 4; col++) {
+      if (
+        board[row][col] &&
+        board[row][col] === board[row - 1][col + 1] &&
+        board[row][col] === board[row - 2][col + 2] &&
+        board[row][col] === board[row - 3][col + 3]
+      ) {
+        return board[row][col];
+      }
+    }
+  }
+
+  return null; // No winner
+}
+
 function Board() {
   const [board, setBoard] = useState(createBoard());
   const [turn, setTurn] = useState("red");
+  let winner = checkWin(board);
 
   function click(col) {
+    if (winner) {
+      return;
+    }
     const colItems = board[col];
     let newBoard = [...board];
     for (let i = 5; i >= 0; i--) {
@@ -26,7 +90,7 @@ function Board() {
         break;
       }
     }
-    console.log(newBoard)
+    console.log(newBoard);
     setBoard(newBoard);
     setTurn(turn === "red" ? "blue" : "red");
   }
@@ -45,8 +109,9 @@ function Board() {
             ))}
           </div>
         ))}
-        {/* {winner && <div className="winner">{winner.toUpperCase()} wins!</div>} */}
+        {winner && <div className="winner">{winner.toUpperCase()} wins!</div>}
       </div>
+      <button className="btn hidden">Play again</button>
     </>
   );
 }
